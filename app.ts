00d2d923be7673fc72f3561ba97867351b45dc06
pemrogramanwebdenganframework <lusita.14ayumardiansah@gmail.com>
2020-12-18 02:13:18 +0700

    menambahkan proses

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
 //1. import modul application dari https://deno.land/x/oak/mod.ts
-import{Apllication} from 'https://deno.land/x/oak/mod.ts';
+import{ Application } from 'https://deno.land/x/oak/mod.ts';
 
 //2. new instance application ke variable kita
-const app = Apllication();
+const app = new Application();
 
 //3. definisikan middleware app kita
 
@@ -11,3 +11,6 @@ app.use((ctx)=>{
 
 });
 
+//4. jalankan server
+console.log("service berjalan di port 8000");
+await app.listen({port :8000});
