056c15970c27c7f2b0f568b658cbc1892bf97d91
pemrogramanwebdenganframework <lusita.14ayumardiansah@gmail.com>
2020-12-18 08:50:49 +0700

    membuat new instan

--- rute.ts
+++ rute.ts
@@ -1,1 +1,15 @@
 import{ router } from 'https://deno.land/x/oak/mod.ts';
+
+const router = new Router();
+
+router
+    .get("/", (ctx)=>{
+        ctx.response.body = "ini halaman home";
+    })
+    .get("/kategori", (ctx)=>{
+        ctx.response.body = "ini halaman kategori";
+    })
+    .get("/about", (ctx)=>{
+        ctx.response.body = "ini halaman About";
+    });
+export default router;
