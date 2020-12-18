d52109bffc34f77585f8de7724cabb8a688192fd
pemrogramanwebdenganframework <lusita.14ayumardiansah@gmail.com>
2020-12-18 21:23:07 +0700

    file rute.ts

--- rute.ts
+++ rute.ts
@@ -1,10 +1,11 @@
 import{ Router } from 'https://deno.land/x/oak/mod.ts';
-
+import {renderFileToString} from 'https://deno.land/x/dejs/mod.ts';
 const router = new Router();
 
 router
-    .get("/", (ctx)=>{
-        ctx.response.body = "ini halaman home";
+    .get("/",async (ctx)=>{
+        const html= await renderFileToString("./views/home.ejs",{});
+        ctx.response.body = new TextEncoder().encode(html);
     })
     .get("/kategori", (ctx)=>{
         ctx.response.body = "ini halaman kategori";
