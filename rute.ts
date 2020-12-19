f3413b97616bc614fec8be3ff91a0e2dec174e1f
pemrogramanwebdenganframework <lusita.14ayumardiansah@gmail.com>
2020-12-19 18:30:21 +0700

    modifikasi file home.ejs

--- rute.ts
+++ rute.ts
@@ -4,7 +4,18 @@ const router = new Router();
 
 router
     .get("/",async (ctx)=>{
-        const html= await renderFileToString("./views/home.ejs",{});
+        const html= await renderFileToString("./views/home.ejs",{
+            data :{
+                nama : "lusita ayu mardiansah",
+                pemrograman : ["PHP","Typescript","Javascript"],
+                mahasiswa : [
+                    {nim:"1", "nama" : "batis"},
+                    {nim:"2", "nama" : "santi"},
+                    {nim:"3", "nama" : "anisya"}
+                ]
+            }
+        });
+        
         ctx.response.body = new TextEncoder().encode(html);
     })
     .get("/kategori", (ctx)=>{
