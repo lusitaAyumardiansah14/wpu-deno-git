946801c83aba9bfeb892fd1c30f3f583a276f506
pemrogramanwebdenganframework <lusita.14ayumardiansah@gmail.com>
2020-12-20 00:55:14 +0700

    membuat controller file rute.ts

--- rute.ts
+++ rute.ts
@@ -1,23 +1,9 @@
 import{ Router } from 'https://deno.land/x/oak/mod.ts';
-import {renderFileToString} from 'https://deno.land/x/dejs/mod.ts';
+import{home} from './controller/blog.ts';
 const router = new Router();
 
 router
-    .get("/",async (ctx)=>{
-        const html= await renderFileToString("./views/home.ejs",{
-            data :{
-                nama : "lusita ayu mardiansah",
-                pemrograman : ["PHP","Typescript","Javascript"],
-                mahasiswa : [
-                    {nim:"1", "nama" : "batis"},
-                    {nim:"2", "nama" : "santi"},
-                    {nim:"3", "nama" : "anisya"}
-                ]
-            }
-        });
-        
-        ctx.response.body = new TextEncoder().encode(html);
-    })
+    .get("/",home)
     .get("/kategori", (ctx)=>{
         ctx.response.body = "ini halaman kategori";
     })
