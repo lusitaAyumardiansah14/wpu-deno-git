bd6c7376d83cecef56ff55904e48acaeadb06bbd
pemrogramanwebdenganframework <lusita.14ayumardiansah@gmail.com>
2020-12-26 16:48:20 +0700

    modifikasi pada file controller/blog.ts

--- models/sql.ts
+++ models/sql.ts
@@ -3,7 +3,7 @@ interface ISql {
     
 }
 const TSql = {} as ISql;
-TSql['ktgFindall'] = "select * from tbl_kategori;";
+TSql['ktgFindAll'] = "select * from tbl_kategori;";
 TSql['ktgFindByKode'] = "select * from tbl_kategori where kode = $1;";
 TSql['ktgFindInKode'] = "select * from tbl_kategori where kode ini ($1, $2,$3);";
 
